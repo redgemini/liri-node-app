f02cf445f413cb467eb7d807c812202dd053e9a0
redgemini <wendykiddy@Wendys-MacBook-Pro.local>
2018-04-06 21:34:06 -0500

    twitterAdds

--- liri.js
+++ liri.js
@@ -1,1 +1,35 @@
+console.log ("liri loaded")
+
 require("dotenv").config();
+
+
+let request = require("request-promise");
+let fs = require("fs");
+let command = process.argv[2];
+let keys = require("./keys.js");
+var Twitter = require("Twitter");
+//var Spotify = require("");
+//var spotify = new Spotify(keys.spotify);
+
+if (command === "my-tweets") {
+var client = new Twitter(keys.twitter);
+
+  console.log(true);
+  var params = { count:20, include_rts: true };
+  client.get("statuses/user_timeline", params, function(error,tweets,response) {
+    if (!error) {
+      for(var i=0; i< tweets.length; i++) {
+        console.log(tweets[i].created_at);
+        console.log("")
+        console.log(tweets[i].text);
+      }
+		}
+	})
+  }
+
+
+
+
+
+
+
